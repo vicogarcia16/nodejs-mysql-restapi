12fa4407fb62f0d78db959391b73ba2c815d313a
vmgn16 <mg_619@hotmail.com>
2024-12-11 00:36:52 -0600

    ignorar certificado autofirmado

--- src/db.js
+++ src/db.js
@@ -7,5 +7,8 @@ export const db = createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    port: process.env.DB_DOCKER_PORT
+    port: process.env.DB_DOCKER_PORT,
+    ssl: {
+        rejectUnauthorized: false,
+      },
 });
