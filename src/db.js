fbba3f57cc92760ef438f8a670761c5d0212dc2f
vmgn16 <mg_619@hotmail.com>
2024-12-11 00:22:41 -0600

    add port

--- src/db.js
+++ src/db.js
@@ -6,5 +6,6 @@ export const db = createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_DOCKER_PORT
 });
